1123d79d1615918775605b06df7d9b1c3cc08d87
tadaken3 <k.tanaka6057@gmail.com>
2018-07-14 12:37:40 +0900

    エディタ画面作成

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true
+    },
+    "extends": "eslint:recommended",
+    "plugin": "vue/recommended"
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 5
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,24 +1,47 @@
 <template>
-　 <div class="editor">
-　 <h1>エディター画面</h1>
-   <span>{{ user.displayName }}</span>
-   <button @click="logout"> ログアウト</button>
-　 </div>
+<div class="editor">
+  <h1>エディター画面</h1>
+  <span>{{ user.displayName }}</span>
+  <button @click="logout"> ログアウト</button>
+  <div class="editorWrapper">
+    <textarea class="markdown" v-model="markdown"></textarea>
+    <div class="preview" v-html="preview()"></div>
+  </div>
+</div>
 </template>
 
 <script>
+import marked from 'marked';
 export default {
-　 name: 'editor',
-　 props: ['user'],
-　 data() {
-　 return {}
-　 },
-　 methods: {
-　 logout: function() {
-　 firebase.auth().signOut();
-　 }
-　}
+  name: 'editor',
+  props: ['user'],
+  data() {
+    return {
+        markdown:'',
+    }
+   },
+   methods: {
+   logout: function() {
+        firebase.auth().signOut();
+   },
+   preview: function() {
+        return marked(this.markdown);
+   },
+   
+ }
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.editorWrapper{
+    display:flex;
+}
+.markdown {
+    width: 50%;
+    height: 500px;
+}
+.prevew{
+    width: 50%;
+    text-align: left;
+}
+</style>
